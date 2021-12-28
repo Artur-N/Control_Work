db318824105d02bcdbead2bca997ca8c661c07be
Artur <arturnl@ya.ru>
2021-12-28 20:26:40 +0300

    Write code

--- Dev1/Program.cs
+++ Dev1/Program.cs
@@ -1,2 +1,50 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Написать программу, которая из имеющегося массива целых чисел формирует массив из четных чисел. 
+// Первоначальный массив можно ввести с клавиатуры, либо сгенерировать случайным образом. 
+
+Random rnd = new Random();
+void FillArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rnd.Next(-10, 10);
+    }
+}
+
+int j = 0;
+int[] EvenNumbers(int[] array)
+{
+    int[] array2 = new int[array.Length];
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0)
+        {
+            array2[j] = array[i];
+            j++;
+        }
+    }
+    if (j == 0) Console.WriteLine("Чётных чисел в данном массиве нет!");
+    return array2;
+}
+
+void PrintRndArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+}
+
+void PrintEvenArray(int[] array)
+{
+    for (int i = 0; i < j; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+}
+
+int[] RndArray = new int[10];
+
+FillArray(RndArray);
+PrintRndArray(RndArray);
+System.Console.WriteLine();
+PrintEvenArray(EvenNumbers(RndArray));
